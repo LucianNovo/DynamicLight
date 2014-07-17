fe9793ba213ca84f46d3bad892abb09fa429ecc3
LucianNovo <luciannovo@gmail.com>
2014-07-17 03:30:46 -0700

    Completed a partially blinded version of the septoclops. Circuit now updates lights to dim as user approaches.

---
+++ ArduinoExamples/ultrasoundSensor/megaTest/septoclops_segmentedLights/septoclops_segmentedLights.ino
@@ -0,0 +1,91 @@
+// ---------------------------------------------------------------------------
+// This example code was used to successfully communicate with 15 ultrasonic sensors. You can adjust
+// the number of sensors in your project by changing SONAR_NUM and the number of NewPing objects in the
+// "sonar" array. You also need to change the pins for each sensor for the NewPing objects. Each sensor
+// is pinged at 33ms intervals. So, one cycle of all sensors takes 495ms (33 * 15 = 495ms). The results
+// are sent to the "oneSensorCycle" function which currently just displays the distance data. Your project
+// would normally process the sensor results in this function (for example, decide if a robot needs to
+// turn and call the turn function). Keep in mind this example is event-driven. Your complete sketch needs
+// to be written so there's no "delay" commands and the loop() cycles at faster than a 33ms rate. If other
+// processes take longer than 33ms, you'll need to increase PING_INTERVAL so it doesn't get behind.
+// ---------------------------------------------------------------------------
+#include <NewPing.h>
+#include <Adafruit_NeoPixel.h>
+
+#define SONAR_NUM     6 // Number of sensors.
+#define MAX_DISTANCE 900 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+int LED_pin = 2;
+int segments = 6;
+unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
+unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
+uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, LED_pin, NEO_GRB + NEO_KHZ800);
+
+NewPing sonar[SONAR_NUM] = {     // Sensor object array.
+
+  //Side 1  of the the septoclops
+  NewPing(22, 23, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(24, 25, MAX_DISTANCE),
+  NewPing(26, 27, MAX_DISTANCE),
+  
+  //Side 2  of the septoclops
+  NewPing(32, 33, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(34, 35, MAX_DISTANCE),
+  NewPing(36, 37, MAX_DISTANCE),
+};
+
+void setup() {
+  //Selects pin to write LED strip data to 
+  pinMode(2, OUTPUT);//2 is output pin
+  //Initiates and clears all LED settings
+  strip.show();
+  
+  //Begin serial
+  Serial.begin(115200);
+  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+}
+
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+    }
+  }
+  // Other code that *DOESN'T* analyze ping results can go here.
+}
+
+void echoCheck() { // If ping received, set the sensor distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+
+void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
+  // The following code would be replaced with your code that does something with the ping results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    Serial.print(i);
+    Serial.print("=");
+    Serial.print(cm[i]);
+    Serial.print("cm ");
+    
+    
+    int temp = int(cm[i]);
+    int result = map(temp, 0, MAX_DISTANCE, 0, 255);
+    
+    //Write the distance data to it's coresponding series
+    for(int j = (10*i); j<(10*(i+1)); j++){
+      strip.setPixelColor(j,strip.Color(result, result, result));
+      Serial.print("Wrote to: " + j);
+      strip.show();
+    }
+  }
+  Serial.println();
+}
